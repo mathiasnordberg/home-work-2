beab6e7f26d8803a16611b488a9bf6980ed4a0cc
mathiasnordberg <mathiasnordberg@hotmail.com>
2015-03-20 14:18:52 +0100

    Update and rename AbstractItem.java to AbstractClass.java

---
+++ AbstractClass.java
@@ -0,0 +1,20 @@
+package inlamning2;
+
+/**
+ *
+ * @author 96matnor
+ */
+public abstract class AbstractClass implements Comparable<AbstractClass>{
+    
+    public AbstractClass(){
+        
+    }
+    /**
+     * 
+     * @return sträng med namnet
+     * toString gör objektet till en sträng
+     */
+    public abstract String getName();
+    @Override
+    public abstract String toString();
+}
