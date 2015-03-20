21b0fb7bcc9d55ff177ceed63ea6d38f423af784
mathiasnordberg <mathiasnordberg@hotmail.com>
2015-03-20 14:18:26 +0100

    Update and rename RegisterGUI.java to GUI.java

---
+++ GUI.java
@@ -0,0 +1,366 @@
+package inlamning2;
+import java.io.*;
+import javax.swing.*;
+
+public class GUI extends javax.swing.JFrame {
+    
+    /**
+     * @author 96matnor
+     */
+    public GUI() {
+        initComponents();
+        m = new Model();
+    }
+   
+    FileWriter skrivFil1;
+    Model m;
+    
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jPanel1 = new javax.swing.JPanel();
+        btn_add = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        txf_author = new javax.swing.JTextField();
+        txf_name = new javax.swing.JTextField();
+        txf_year = new javax.swing.JTextField();
+        cmb_type = new javax.swing.JComboBox();
+        btn_savefile = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txa_register = new javax.swing.JTextArea();
+        jLabel3 = new javax.swing.JLabel();
+        btn_read = new javax.swing.JButton();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        menu_open = new javax.swing.JMenuItem();
+        menu_save = new javax.swing.JMenuItem();
+        menu_quit = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        menu_about = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Inlämning_2");
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+
+        btn_add.setText("Lägg till objekt");
+        btn_add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_addActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Namn: ");
+
+        jLabel2.setText("Författare/Utgivare: ");
+
+        jLabel4.setText("År: ");
+
+        cmb_type.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Bok", "Tidning" }));
+
+        btn_savefile.setText("Spara fil");
+        btn_savefile.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_savefileActionPerformed(evt);
+            }
+        });
+
+        org.jdesktop.layout.GroupLayout jPanel1Layout = new org.jdesktop.layout.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel1Layout.createSequentialGroup()
+                .add(24, 24, 24)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jLabel2)
+                    .add(jLabel4)
+                    .add(jLabel1))
+                .add(74, 74, 74)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(txf_name, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 120, Short.MAX_VALUE)
+                    .add(txf_author)
+                    .add(txf_year))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING, false)
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, cmb_type, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, btn_add, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, btn_savefile, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jLabel1)
+                    .add(txf_name, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(cmb_type, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .add(18, 18, 18)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jLabel2)
+                    .add(txf_author, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 23, Short.MAX_VALUE)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(jLabel4)
+                    .add(txf_year, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(btn_add))
+                .add(24, 24, 24)
+                .add(btn_savefile))
+        );
+
+        txa_register.setColumns(20);
+        txa_register.setRows(5);
+        jScrollPane1.setViewportView(txa_register);
+
+        jLabel3.setText("Register");
+
+        btn_read.setText("Läs in fil");
+        btn_read.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_readActionPerformed(evt);
+            }
+        });
+
+        org.jdesktop.layout.GroupLayout jPanel2Layout = new org.jdesktop.layout.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 521, Short.MAX_VALUE)
+                    .add(jPanel2Layout.createSequentialGroup()
+                        .add(jLabel3)
+                        .add(0, 0, Short.MAX_VALUE))
+                    .add(jPanel2Layout.createSequentialGroup()
+                        .add(0, 0, Short.MAX_VALUE)
+                        .add(btn_read, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 100, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel2Layout.createSequentialGroup()
+                .add(9, 9, 9)
+                .add(jLabel3)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 151, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .add(btn_read)
+                .add(19, 19, 19))
+        );
+
+        jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+
+        jMenu1.setText("Akriv");
+
+        menu_open.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.event.InputEvent.CTRL_MASK));
+        menu_open.setText("Öppna bibliotek");
+        menu_open.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menu_openActionPerformed(evt);
+            }
+        });
+        jMenu1.add(menu_open);
+
+        menu_save.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
+        menu_save.setText("Spara");
+        menu_save.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menu_saveActionPerformed(evt);
+            }
+        });
+        jMenu1.add(menu_save);
+
+        menu_quit.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_F4, java.awt.event.InputEvent.ALT_MASK));
+        menu_quit.setText("Avsluta");
+        menu_quit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menu_quitActionPerformed(evt);
+            }
+        });
+        jMenu1.add(menu_quit);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Om");
+
+        menu_about.setText("Om programmet");
+        menu_about.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menu_aboutActionPerformed(evt);
+            }
+        });
+        jMenu2.add(menu_about);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
+
+        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jPanel1, 0, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .add(jPanel2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jPanel2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+        /**
+         * Lägger till Namn, författare och år i biblioteket
+         * @param evt 
+         * 
+         */
+    private void btn_addActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_addActionPerformed
+        m.add(txf_name.getText(),txf_author.getText(),Integer.parseInt(txf_year.getText()), cmb_type.getSelectedIndex());
+        txf_name.setText("");
+        txf_author.setText("");
+        txf_year.setText("");
+    }//GEN-LAST:event_btn_addActionPerformed
+        /**
+         * Öppnar en file chooser och läser sedan in csv filen du valt.
+         * @param evt 
+         * 
+         */
+    private void btn_readActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_readActionPerformed
+        JFileChooser fc = new JFileChooser(System.getProperty("user.dir"));
+         fc.showDialog(null, "Välj fil");
+         try{
+         String fileName = fc.getSelectedFile().getName(); 
+         m.read_file(fileName, txa_register);
+         }catch(NullPointerException e){
+             
+         }
+        
+    }//GEN-LAST:event_btn_readActionPerformed
+        /**
+         * Sparar csv filen.
+         * @param evt 
+         * 
+         */
+    private void btn_savefileActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_savefileActionPerformed
+        String fileName = JOptionPane.showInputDialog("Namn på filen:");
+        m.save_file(fileName);
+    }//GEN-LAST:event_btn_savefileActionPerformed
+        /**
+         * Öppnar en file chooser och läser sedan in csv filen du valt.
+         * @param evt 
+         * 
+         */
+    private void menu_openActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_openActionPerformed
+        JFileChooser fc = new JFileChooser(System.getProperty("user.dir"));
+         fc.showDialog(null, "Välj fil");
+          try{
+         String fileName = fc.getSelectedFile().getName(); 
+         m.read_file(fileName, txa_register);
+         }catch(NullPointerException e){
+             
+         }
+    }//GEN-LAST:event_menu_openActionPerformed
+        /**
+         * Sparar csv filen.
+         * @param evt 
+         * 
+         */
+    private void menu_saveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_saveActionPerformed
+        String fileName = JOptionPane.showInputDialog("Namn på filen:");
+        m.save_file(fileName);
+    }//GEN-LAST:event_menu_saveActionPerformed
+        /**
+         * Avslutar programmet.
+         * @param evt 
+         * 
+         */
+    private void menu_quitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_quitActionPerformed
+        System.exit(2);
+    }//GEN-LAST:event_menu_quitActionPerformed
+        /**
+         * Öppnar ett meddelande om programmet.
+         * @param evt 
+         * 
+         */
+    private void menu_aboutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menu_aboutActionPerformed
+        JOptionPane.showMessageDialog(null,"Version 1.0 Skapat av: Mathias Nordberg");
+    }//GEN-LAST:event_menu_aboutActionPerformed
+    
+    
+    
+    
+    
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            javax.swing.UIManager.LookAndFeelInfo[] installedLookAndFeels=javax.swing.UIManager.getInstalledLookAndFeels();
+            for (int idx=0; idx<installedLookAndFeels.length; idx++)
+                if ("Nimbus".equals(installedLookAndFeels[idx].getName())) {
+                    javax.swing.UIManager.setLookAndFeel(installedLookAndFeels[idx].getClassName());
+                    break;
+                }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GUI().setVisible(true);
+            }
+        });
+    }
+    
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btn_add;
+    private javax.swing.JButton btn_read;
+    private javax.swing.JButton btn_savefile;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JComboBox cmb_type;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JMenuItem menu_about;
+    private javax.swing.JMenuItem menu_open;
+    private javax.swing.JMenuItem menu_quit;
+    private javax.swing.JMenuItem menu_save;
+    private javax.swing.JTextArea txa_register;
+    private javax.swing.JTextField txf_author;
+    private javax.swing.JTextField txf_name;
+    private javax.swing.JTextField txf_year;
+    // End of variables declaration//GEN-END:variables
+    
+}
